68510dbdc517791d7333e75d4a0a37508c90ab68
HERNANDEZ CORTES, JOSE ANTONIO <josehernandezco@externos.santander.com.mx>
2022-03-24 02:15:51 -0600

    se agregan interfaces del api gramificacion y se modifica el servicio

--- src/app/services/apis/gamification.service.ts
+++ src/app/services/apis/gamification.service.ts
@@ -1,10 +1,20 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+import { GamificationDetailResponse } from "../interfaces/gamification.interfaces";
 
 @Injectable({
     providedIn: 'root'
 })
 export class GamificationService {
 
-    private _apiUrl: string = "";
+    private _getApiUri: string = "/v1/gamifications/{Gamification-key}";
+
+    constructor( private httpClient: HttpClient ) { }
+
+    getGamifications(): Observable<GamificationDetailResponse> {
+        return this.httpClient.get<GamificationDetailResponse>(`http:127.0.0.1:8083${this._getApiUri}`);
+    }
 
 }
