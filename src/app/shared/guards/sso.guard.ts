93163353fa2af09b43949d940a5f2ade2283e779
HERNANDEZ CORTES, JOSE ANTONIO <josehernandezco@externos.santander.com.mx>
2022-04-12 19:00:17 -0500

    se agrega el mock de respuesta del single sign on para validar el token

--- src/app/shared/guards/sso.guard.ts
+++ src/app/shared/guards/sso.guard.ts
@@ -1,16 +1,14 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { SsoFacade } from 'src/app/services/facades/sso.facade';
+import { TokenSsoFacade } from 'src/app/services/facades/sso.facade';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SsoGuard implements CanActivate, CanLoad {
 
-  constructor(private ssoFacade:SsoFacade){
-
-  }
+  constructor( private ssoFacade: TokenSsoFacade ) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
