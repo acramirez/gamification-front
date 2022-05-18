abddff472335cdbed2e0c34c79e7112aec9c2969
mi <>
2022-05-18 17:49:12 -0500

    correcion bug

--- src/app/services/facades/sso.facade.ts
+++ src/app/services/facades/sso.facade.ts
@@ -34,7 +34,6 @@ export class TokenSsoFacade {
         this._token=tkn
         
         return of(true)
-        return this.tokenService.getValidateToken(tkn)
     }
 
     isBase64Token(tkn:string){
