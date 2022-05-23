563ecea42550c0a430ba51772d27b1962d148c7c
mi <>
2022-05-23 18:39:14 -0500

    se generan los mensajes de acuerdo al periodo anterior

--- src/app/pages/message/message.component.ts
+++ src/app/pages/message/message.component.ts
@@ -1,11 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Notification } from "../../shared/interfaces/notification";
 
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
   styleUrls: ['./message.component.css']
 })
-export class MessageComponent {
+export class MessageComponent implements OnInit {
+
+  notification:Notification={
+    icon:'',
+    title:'',
+    subtitle:'',
+    description:''
+  }
+
+  constructor(
+    private activatedRoute:ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+    let message = ''
+
+    this.activatedRoute.params.subscribe(param=>{
+      message=param.message
+    })
+
+    console.log(message);
+    
+    switch (message) {
+      case 'lo-sentimos':
+        this.notification.icon='challenge-no-complete'
+        this.notification.title='¡Lo sentimos!'
+        this.notification.subtitle='No has logrado completar el reto LikeU'
+        this.notification.description='Desafortunadamente no podrás continuar participando en el reto. Recuerda que puedes continuar usando tu tarjeta.'
+
+        break;
+      case 'mision-cumplida':
+        this.notification.icon='mission-complete'
+        this.notification.title='¡Misión cumplida!'
+        this.notification.subtitle='Estás más cerca de alcanzar tu límite potencial'
+        this.notification.description='Continúa con los retos de la siguiente misión para avanzar.'
+
+        break;
+      case 'lo-has-logrado':
+        this.notification.icon='challenge-complete'
+        this.notification.title='¡Lo has logrado!'
+        this.notification.subtitle='Tu límite de crédito ha aumentado'
+        this.notification.description='Completaste todas las misiones del reto LikeU y tu límite ha alcanzado su potencial completo.'
+
+        break;
+    
+      default:
+        break;
+    }
+
+  }
 
 
 }
---
+++ src/app/shared/interfaces/notification.ts
@@ -0,0 +1,6 @@
+export interface Notification {
+    icon:string,
+    title:string,
+    subtitle:string,
+    description:string
+}
