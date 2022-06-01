8f83d4b7882418427221517cedd29ee90293cd19
mi <>
2022-06-01 17:26:15 -0500

    Correciones sonar

--- src/app/shared/atoms/spinner/spinner.component.ts
+++ src/app/shared/atoms/spinner/spinner.component.ts
@@ -5,11 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.css']
 })
-export class SpinnerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class SpinnerComponent {
 
 }
