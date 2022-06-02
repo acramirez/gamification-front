99a9df4ec495932f361faafd775b7a37205e9e2f
mi <>
2022-06-02 12:58:34 -0500

    prueba

--- src/app/services/apis/config.service.ts
+++ src/app/services/apis/config.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -14,7 +15,13 @@ export class ConfigService {
 
   getConfig(){
     return new Promise((resolve,reject)=>{
-      this.http.get('/config.json').subscribe(
+      this.http.get('/config.json').pipe(
+        map(resp=>{
+          console.log(resp);
+          
+          return JSON.stringify(resp)
+        })
+      ).subscribe(
         data=>{
           console.log(data);
           resolve(true)
