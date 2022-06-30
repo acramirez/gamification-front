30ef1c1b4e8ba880a84334b7a608879201bf78f7
Z353440 <z353440@smmacboz353440m.gfsscorp.mx.bsch>
2022-06-30 12:32:52 -0500

    proxy para local

--- proxy.conf.js
+++ proxy.conf.js
@@ -2,12 +2,19 @@ const PROXY_CONFIG = [
   {
       context: [
           "/api/session",
-          "/credits/v2/credits/customer-key/benefits"
       ],
-      target: "https://mx-gamification-front-mx-gamification-web-dev.appls.mex01.mex.dev.mx1.paas.cloudcenter.corp",
+      target: "https://gamification-service-sec-mx-gamification-web-dev.appls.mex01.mex.dev.mx1.paas.cloudcenter.corp/api/session",
       secure: false,
       changeOrigin: true
-  }
+  },
+  {
+      context: [
+          "/credits/v2/credits/customer-key/benefits"
+      ],
+      target: "https://gamification-service-mx-gamification-dev.appls.mex01.mex.dev.mx1.paas.cloudcenter.corp/credits/v2/credits/customer-key/benefits",
+      secure: false,
+      changeOrigin: true
+  },
 ]
 
 module.exports = PROXY_CONFIG;
