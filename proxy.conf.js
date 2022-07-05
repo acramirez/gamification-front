bd2c1a1ebcf3c2e7b9f4e5b60018db52c8030bca
Z353440 <z353440@smmacboz353440m.gfsscorp.mx.bsch>
2022-07-05 18:10:54 -0500

    correccion sonar

--- proxy.conf.js
+++ proxy.conf.js
@@ -7,7 +7,7 @@ const PROXY_CONFIG = [
       target: "https://mx-gamification-front-mx-gamification-web-dev.appls.mex01.mex.dev.mx1.paas.cloudcenter.corp",
       secure: false,
       changeOrigin: true
-  },
+  }
 ]
 
 module.exports = PROXY_CONFIG;
