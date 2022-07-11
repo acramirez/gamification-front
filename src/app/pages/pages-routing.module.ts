66ff654079aeb6edc8144549c0e9028179e0e8e4
Z353440 <z353440@smmacboz353440m.gfsscorp.mx.bsch>
2022-07-11 12:29:53 -0500

    correccion de rutas

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -12,6 +12,10 @@ const routes: Routes = [
       {
         path:'',
         component:ChallengeLikeuComponent
+      },
+      {
+        path:'**',
+        redirectTo:'/'
       }
     ],
     // canLoad:[SsoGuard],
